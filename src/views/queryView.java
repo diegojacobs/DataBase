6adca380fa4975c9bfbd996d32d4b6058e842ade
Kevin García <gar13177@uvg.edu.gt>
2016-03-18 18:01:10 -0600

    Ventana con los componentes principales agregados
    Se agregaron nuevos botones, dos ventanas para escribir queries y
    resultados

--- src/views/queryView.java
+++ src/views/queryView.java
@@ -16,6 +16,12 @@ import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.border.Border;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
 
 public class queryView {
 
@@ -71,17 +77,7 @@ public class queryView {
 		JToolBar toolBar = new JToolBar();
 		frame.getContentPane().add(toolBar, BorderLayout.NORTH);
 		
-		JButton btnRun = new JButton("Run");
-		try{
-			Image img = ImageIO.read(getClass().getClassLoader().getResource("resources/images/1playButton.png"));
-			btnRun.setIcon(new ImageIcon(img));
-			btnRun.setText("");
-			Border emptyBorder = BorderFactory.createEmptyBorder();
-			btnRun.setBorder(emptyBorder);
-		} catch (Exception e){
-			System.out.println("Error in resources/images/1playButton.png");
-		}
-		toolBar.add(btnRun);
+		
 		
 		JButton btnOpenFile = new JButton("Open File");
 		try{
@@ -94,6 +90,89 @@ public class queryView {
 			System.out.println("Error in resources/images/2openButton.png");
 		}
 		toolBar.add(btnOpenFile);
+		
+		JButton btnSave = new JButton("Save");
+		try{
+			Image img = ImageIO.read(getClass().getClassLoader().getResource("resources/images/3saveButton.png"));
+			btnSave.setIcon(new ImageIcon(img));
+			btnSave.setText("");
+			Border emptyBorder = BorderFactory.createEmptyBorder();
+			btnSave.setBorder(emptyBorder);
+			btnSave.setEnabled(false);//false until a file is written
+		} catch (Exception e){
+			System.out.println("Error in resources/images/3saveButton.png");
+		}
+		toolBar.add(btnSave);
+		
+		JButton btnCut = new JButton("Cut");
+		try{
+			Image img = ImageIO.read(getClass().getClassLoader().getResource("resources/images/4cutButton.png"));
+			btnCut.setIcon(new ImageIcon(img));
+			btnCut.setText("");
+			Border emptyBorder = BorderFactory.createEmptyBorder();
+			btnCut.setBorder(emptyBorder);
+		} catch (Exception e){
+			System.out.println("Error in resources/images/4cutButton.png");
+		}
+		toolBar.add(btnCut);
+		
+		JButton btnCopy = new JButton("Copy");
+		try{
+			Image img = ImageIO.read(getClass().getClassLoader().getResource("resources/images/5copyButton.png"));
+			btnCopy.setIcon(new ImageIcon(img));
+			btnCopy.setText("");
+			Border emptyBorder = BorderFactory.createEmptyBorder();
+			btnCopy.setBorder(emptyBorder);
+		} catch (Exception e){
+			System.out.println("Error in resources/images/5copyButton.png");
+		}
+		toolBar.add(btnCopy);
+		
+		JButton btnPaste = new JButton("Paste");
+		try{
+			Image img = ImageIO.read(getClass().getClassLoader().getResource("resources/images/6pasteButton.png"));
+			btnPaste.setIcon(new ImageIcon(img));
+			btnPaste.setText("");
+			Border emptyBorder = BorderFactory.createEmptyBorder();
+			btnPaste.setBorder(emptyBorder);
+		} catch (Exception e){
+			System.out.println("Error in resources/images/6pasteButton.png");
+		}
+		toolBar.add(btnPaste);
+		
+		JButton btnRun = new JButton("Run");
+		try{
+			Image img = ImageIO.read(getClass().getClassLoader().getResource("resources/images/1playButton.png"));
+			btnRun.setIcon(new ImageIcon(img));
+			btnRun.setText("");
+			Border emptyBorder = BorderFactory.createEmptyBorder();
+			btnRun.setBorder(emptyBorder);
+		} catch (Exception e){
+			System.out.println("Error in resources/images/1playButton.png");
+		}
+		toolBar.add(btnRun);
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setResizeWeight(0.5);
+		splitPane.setContinuousLayout(true);
+		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		frame.getContentPane().add(splitPane, BorderLayout.CENTER);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		splitPane.setLeftComponent(tabbedPane);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		tabbedPane.addTab("SQL Editor", null, scrollPane_1, null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setTabSize(4);
+		scrollPane_1.setViewportView(textArea);
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		splitPane.setRightComponent(tabbedPane_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		tabbedPane_1.addTab("Data Output", null, scrollPane, null);
 		
 	}
 
